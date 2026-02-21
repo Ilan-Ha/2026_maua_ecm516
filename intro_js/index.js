7f7f01401ade04a0412130eb19eea118d7f9c959
ILAN HAMEIRY <23.00981-0@maua.br>
2026-02-21 09:23:30 -0300

    feat(main): creating repository

---
+++ intro_js/index.js
@@ -0,0 +1,54 @@
+// Ctrl k Ctrl c -> coloca //
+// Ctrl k Ctrl u -> tira //
+//declarando constantes
+    /// em javaScript
+        // variável -> ponteiro (variavel de referência, não tem tipo)
+// const nome = 'josé' 
+// console.log(nome)
+// nome = 'José Silva' //(erro, mudando constante)
+
+// const idade = 17
+// console.log(typeof(idade)) // o tipo da valor que a variavel aponta
+
+        // Declarando Variaveis
+//  let a = 2
+//  console.log(a)
+//  let passouDeAno = true
+//  console.log(passouDeAno)
+//  a = 3
+//  console.log(a)
+//  let b
+//  console.log(b)
+//  b = 1.5
+//  console.log(b)
+//  console.log(typeof(b))
+//  b = "abc"
+//  console.log(b)
+//  console.log(typeof(b))
+
+    // var
+// var c = 2
+// c = 3
+// var e antigo em relacao a let e const
+
+     // porque nao e bom usar var mais
+// var linguagem = 'javascript'
+// console.log('Aprendendo '+ linguagem)
+// var linguagem = 'java'
+// console.log(`Aprendendo ${linguagem}`) /// (crase -> operador de interpolacao, colocar entre)
+/// linguagem teve que ser re declarada (var adimite isso so porque pode)
+
+var idade = 18
+console.log(`oi, ${nome}`)
+if(idade >= 18)
+{
+    var nome = 'João'
+    console.log("Parabéns, " + nome + ". Você pode dirigir")
+}
+console.log('Até, '+ nome)
+/// variavel declarada depois consegue ser usada antes, codigo funciona mesmo que "nome" ainda não tenha sido declarada
+
+    /// em java
+// String nome = "José"; -> linguagem estaticamente tipada (feita pelo compilador)
+
+//console.log('Hello, Word')
